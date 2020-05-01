146a388c2cdfa2fe5e3b7ea871c7db76ec437119
Franco Liberali <franco.liberali@gmail.com>
2020-05-01 19:32:06 -0300

    meter logger pino

--- app.js
+++ app.js
@@ -2,24 +2,32 @@ const express = require("express");
 require("./src/db/mongoose");
 const home_routers = require("./src/routers/home");
 const user_routers = require("./src/routers/users");
+const logger = require("./logger").logger;
+const loggerHttp = require("./logger").loggerHttp;
 
 const app = express();
+
 const port = process.env.PORT;
 const router = express.Router();
 
 // middleware que parsea a json todo los request
 app.use(express.json());
 
-router.use(function (req, res, next) {
-  console.log("/" + req.method);
-  next();
-});
+// Logeador de errores en requests
+app.use(loggerHttp);
 
 app.use("/", home_routers);
 app.use("/users", user_routers);
 
 const server = app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
+  logger.info(`Example app listening on port ${port}!`);
+  logger.debug("debug de ejemplo");
+  logger.warn("warn de ejemplo");
+  try {
+    throw new Error("excepcion de ejemplo");
+  } catch (error) {
+    logger.warn(error, "mensaje de que paso de ejemplo");
+  }
 });
 
 module.exports = server;
--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -3,7 +3,9 @@ const gitHubApiAdapter = require("../adapters/GitHubApiAdapter");
 
 module.exports = {
   index: (req, res) => {
+    req.log.warn("ejemplo de logeo en controller, que logea todo el request");
     res.send({ title: "Hello world by NodeJS" });
+    res.log.warn("ejemplo de logeo en controller, que logea todo el response");
   },
   github: (req, res) => {
     return gitHubApiAdapter
